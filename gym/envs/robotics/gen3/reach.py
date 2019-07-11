f7c9db7fec9b821c6717c78c3636eb039c363495
Vincent Zhang <vzhangca@gmail.com>
2019-07-10 19:16:00 -0600

    add the gripper joint to reach env

--- gym/envs/robotics/gen3/reach.py
+++ gym/envs/robotics/gen3/reach.py
@@ -15,7 +15,7 @@ class Gen3ReachEnv(gen3_env.Gen3Env, utils.EzPickle):
             # 'robot0:slide2': 0.0,
         }
         gen3_env.Gen3Env.__init__(
-            self, MODEL_XML_PATH, has_object=False, block_gripper=True, n_substeps=20,
+            self, MODEL_XML_PATH, has_object=False, block_gripper=False, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type=reward_type)
